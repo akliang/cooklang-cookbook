fe2064e649535995f0e15e1fbdb5dcf679ad77eb
akliang <none@none.com>
2022-03-23 02:00:56 +0000

    added login stuff

--- web/urls.py
+++ web/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
   path('', views.index, name='index'),
+  path('login/', views.login, name='login'),
+  path('logout/', views.logout, name='logout'),
   path('register/', views.register, name='register'),
   path('recipe/', views.recipe, name='recipe'),
+  path('v/<str:username>/<str:title>', views.view, name='view'),
 ]
--- web/views.py
+++ web/views.py
@@ -2,14 +2,42 @@ from django.shortcuts import render
 
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import  render, redirect
-from .forms import NewUserForm, ModifyRecipe
-from django.contrib.auth import login
+from .forms import LoginForm, ModifyRecipe
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from .cooklang_processor import process as clprocess
+from django.contrib.auth import authenticate, login
 
 
 def index(request):
+  if request.user.is_authenticated:
     return HttpResponse("Hello, world. You're at the polls index.")
+  else:
+    return HttpResponseRedirect('/login')
+
+def login(request):
+  if request.method == "POST":
+    form = LoginForm(request.POST)
+    if form.is_valid():
+      user = authenticate(request, username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+      if user is not None:
+        auth_login(request, user)
+
+  # at this point, you are either logged in or not
+  if request.user.is_authenticated:
+    if request.POST.get('next'):
+      return HttpResponseRedirect(request.POST.get('next'))
+    else:
+      return HttpResponseRedirect('/')
+  else:
+    login_form = LoginForm()
+    return render(request=request, template_name="web/login.html", context={"form":login_form})
+
+def logout(request):
+  auth_logout(request)
+  return HttpResponseRedirect('/')
 
 def register(request):
   if request.method == "POST":
@@ -23,14 +51,15 @@ def register(request):
     messages.error(request, "Unsuccessful registration. Invalid information.")
   
   form = NewUserForm()
-  return render (request=request, template_name="web/register.html", context={"register_form":form})
+  return render(request=request, template_name="web/register.html", context={"register_form":form})
 
 @login_required
 def recipe(request):
   if request.method == 'POST':
     form = ModifyRecipe(request.POST)
     if form.is_valid():
-      with open(f"./data/{request.user.username}/{form.cleaned_data['title']}.txt", "w") as file:
+      filename = form.cleaned_data['title'].replace(" ","-").lower()
+      with open(f"./data/recipes/{request.user.username}/{filename}.cook", "w") as file:
         file.write(f">> title: {form.cleaned_data['title']}\n")
         file.write(f">> tags: {form.cleaned_data['tags']}\n")
         file.write(form.cleaned_data['recipe'])
@@ -40,3 +69,14 @@ def recipe(request):
     form = ModifyRecipe()
 
   return render(request, 'web/recipe.html', {'form': form})
+
+def view(request, username, title):
+  title = title.lower()
+  data = clprocess(f"./data/recipes/{username}/{title}.cook")
+
+  if request.user.is_authenticated and request.user.username == username:
+    edit = True
+  else:
+    edit = False
+
+  return render(request=request, template_name="web/view.html", context={'data': data, 'edit': edit})
