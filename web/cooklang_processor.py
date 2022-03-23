8a7f2728d3936e9ba7a6c9fa551a4851d9fac735
akliang <none@none.com>
2022-03-23 02:01:04 +0000

    added view-recipe

---
+++ web/cooklang_processor.py
@@ -0,0 +1,71 @@
+import re
+
+def process(recipe):
+  recipe_file = open(recipe, 'r')
+
+  # todo: comments
+
+  ingredients = []
+  cookware = []
+  timers = []
+  meta = []
+  recipe = []
+  for line in recipe_file:
+    ingredients += _do_regex("@", line)
+    cookware += _do_regex("#", line)
+    timers += _do_regex("~", line)
+    if _do_meta(line):
+      meta.append(_do_meta(line))
+    else:
+      recipe.append(line)
+
+  # remove all None elemenets from lists
+  ingredients = list(filter(None, ingredients))
+  cookware = list(filter(None, cookware))
+  timers = list(filter(None, timers))
+  meta = list(filter(None, meta))
+
+  # convert recipe from a list into a string
+  recipe = "\n".join(recipe)
+
+  return {
+    'ingredients': ingredients,
+    'cookware': cookware,
+    'timers': timers,
+    'meta': meta,
+    'recipe': recipe,
+  }
+
+
+def _do_regex(symbol, line):
+  ret = []
+  # get indices of all symbols in the line
+  at_indices = [index for index,value in enumerate(line) if value == symbol]
+
+  # walk through each symbol
+  for num,idx in enumerate(at_indices):
+    # only check the text between two symbols
+    if idx != at_indices[-1]:
+      nowline = line[idx:at_indices[num+1]]
+    else:
+      nowline = line[idx:]
+
+    # check if it's a brace-ingredient
+    if (match := re.search(symbol+"(.*?)\{(.*?)\}", nowline)):
+      ingredient = match.group(1)
+      quantity = re.sub("\%", " ", match.group(2))
+    elif (match := re.search(symbol+"([A-Za-z0-9\-_]*)", nowline)):
+      ingredient = match.group(1)
+      quantity = ''
+    else:
+      print('Error!')
+
+    ret.append((ingredient, quantity))
+
+  return ret
+
+def _do_meta(line):
+  if (match := re.match(">>\s*(.*?):\s*(.*)", line)):
+    return match.groups()
+  else:
+    return None
