5443285bd54762fb474967b4c3d4b23ee06ccdac
akliang <none@none.com>
2022-03-27 20:43:30 +0000

    deleted web app

--- web/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class Chef(models.Model):
-  user = models.OneToOneField(User, on_delete=models.CASCADE)
-  email = models.CharField(max_length=255)
-  last_login = models.DateTimeField(auto_now=True)
