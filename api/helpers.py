bf9a65dc515ffd5413bf614d5273bdd0f393ab69
akliang <none@none.com>
2022-03-27 20:42:23 +0000

    merged cooklang_processor into helper

--- api/helpers.py
+++ api/helpers.py
@@ -1,3 +1,4 @@
+import re
 
 def write_formdata_to_cookfile(request, cleaned_data):
   filename = cleaned_data['title'].replace(" ","-").lower()
@@ -6,3 +7,73 @@ def write_formdata_to_cookfile(request, cleaned_data):
     file.write(f">> tags: {cleaned_data['tags']}\n")
     file.write(cleaned_data['recipe'])
   return filename
+
+
+def cooklang_processor(recipe):
+  recipe_file = open(recipe, 'r')
+
+  # todo: comments
+
+  ingredients = []
+  cookware = []
+  timers = []
+  meta = {}
+  recipe = []
+  for line in recipe_file:
+    ingredients += _cl_do_regex("@", line)
+    cookware += _cl_do_regex("#", line)
+    timers += _cl_do_regex("~", line)
+    if data := _cl_do_meta(line):
+      meta[data[0]] = data[1]
+    else:
+      recipe.append(line)
+
+  # remove all None elemenets from lists
+  ingredients = list(filter(None, ingredients))
+  cookware = list(filter(None, cookware))
+  timers = list(filter(None, timers))
+
+  # convert recipe from a list into a string
+  recipe = "\n".join(recipe)
+
+  return {
+    'ingredients': ingredients,
+    'cookware': cookware,
+    'timers': timers,
+    'meta': meta,
+    'recipe': recipe,
+  }
+
+
+def _cl_do_regex(symbol, line):
+  ret = []
+  # get indices of all symbols in the line
+  at_indices = [index for index,value in enumerate(line) if value == symbol]
+
+  # walk through each symbol
+  for num,idx in enumerate(at_indices):
+    # only check the text between two symbols
+    if idx != at_indices[-1]:
+      nowline = line[idx:at_indices[num+1]]
+    else:
+      nowline = line[idx:]
+
+    # check if it's a brace-ingredient
+    if (match := re.search(symbol+"(.*?)\{(.*?)\}", nowline)):
+      ingredient = match.group(1)
+      quantity = re.sub("\%", " ", match.group(2))
+    elif (match := re.search(symbol+"([A-Za-z0-9\-_]*)", nowline)):
+      ingredient = match.group(1)
+      quantity = ''
+    else:
+      print('Error!')
+
+    ret.append((ingredient, quantity))
+
+  return ret
+
+def _cl_do_meta(line):
+  if (match := re.match(">>\s*(.*?):\s*(.*)", line)):
+    return match.groups()
+  else:
+    return None
