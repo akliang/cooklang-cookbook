f4c562c46a9f46bebd8e0c4bf783c5f2a5433edd
akliang <none@none.com>
2022-04-16 05:00:22 +0000

    updated helper import

--- api/tests/test_cooklang_processor_timers.py
+++ api/tests/test_cooklang_processor_timers.py
@@ -2,7 +2,7 @@ import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 import pytest
 
-from api.helpers import cooklang_processor as c
+from api.helpers.cooklang import cooklang_processor as c
 
 @pytest.fixture
 def recipe(tmp_path):
