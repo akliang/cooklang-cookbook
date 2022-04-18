ca18eb5744aaaec911974e4f1dc80b9fd86e73b1
akliang <none@none.com>
2022-04-18 03:51:50 +0000

    added addrecipe url

--- frontend/constants.js
+++ frontend/constants.js
@@ -2,3 +2,4 @@ const api_url = 'https://cookbook.albertliang.xyz/api';
 
 exports.api_login_url = api_url + '/api_login/';
 exports.api_myrecipes_url = api_url + '/view/';
+exports.api_addrecipe_url = api_url + '/add/';
