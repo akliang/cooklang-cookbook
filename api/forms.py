8ed7294536ddfaad431ca08a7d401adca2a2c145
akliang <none@none.com>
2022-04-18 03:53:23 +0000

    updatedrecipe form

--- api/forms.py
+++ api/forms.py
@@ -2,63 +2,43 @@ from django import forms
 from django.contrib.auth.models import User
 from api.models import Chef
 
+# class RegisterForm(forms.Form):
+#   username = forms.CharField(
+#     max_length=255,
+#     widget=forms.TextInput(attrs={'placeholder': 'Username'}),
+#     required=True,
+#   )
 
-# Create your forms here.
+#   email = forms.EmailField(
+#     max_length=255,
+#     widget=forms.EmailInput(attrs={'placeholder': 'Email'}),
+#     required=True,
+#   )
 
-class LoginForm(forms.Form):
-  username = forms.CharField(
-    max_length=255,
-    widget=forms.TextInput(attrs={'placeholder': 'Username', 'autofocus': 'autofocus'}),
-    required=True,
-  )
+#   password = forms.CharField(
+#     max_length=255,
+#     widget=forms.PasswordInput(attrs={'placeholder': 'Password'}),
+#     required=True
+#   )
 
-  password = forms.CharField(
-    max_length=255,
-    widget=forms.PasswordInput(attrs={'placeholder': 'Password'}),
-    required=True
-  )
+#   password2 = forms.CharField(
+#     max_length=255,
+#     widget=forms.PasswordInput(attrs={'placeholder': 'Confirm password'}),
+#     required=True
+#   )
 
-class RegisterForm(forms.Form):
-  username = forms.CharField(
-    max_length=255,
-    widget=forms.TextInput(attrs={'placeholder': 'Username'}),
-    required=True,
-  )
+#   def save(self):
+    # user = User.objects.create_user(username=self.cleaned_data['username'], password=self.cleaned_data['password'])
+    # Chef.objects.create(user=user, email=self.cleaned_data['email'])
+    # return user
 
-  email = forms.EmailField(
-    max_length=255,
-    widget=forms.EmailInput(attrs={'placeholder': 'Email'}),
-    required=True,
-  )
-
-  password = forms.CharField(
-    max_length=255,
-    widget=forms.PasswordInput(attrs={'placeholder': 'Password'}),
-    required=True
-  )
-
-  password2 = forms.CharField(
-    max_length=255,
-    widget=forms.PasswordInput(attrs={'placeholder': 'Confirm password'}),
-    required=True
-  )
-
-  def save(self):
-    user = User.objects.create_user(username=self.cleaned_data['username'], password=self.cleaned_data['password'])
-    Chef.objects.create(user=user, email=self.cleaned_data['email'])
-    return user
-
-class ModifyRecipe(forms.Form):
+class RecipeForm(forms.Form):
   title = forms.CharField(
-    max_length=255,
-    widget=forms.TextInput(attrs={'placeholder': 'Recipe Title'}),
-    required=True
+    max_length=255
   )
   tags = forms.CharField(
     max_length=255,
-    widget=forms.TextInput(attrs={'placeholder': 'Tags'})
+    required=False
   )
-  recipe = forms.CharField(
-    widget=forms.Textarea(attrs={'placeholder': 'Recipe goes here'})
-  )
+  recipe = forms.CharField()
 
