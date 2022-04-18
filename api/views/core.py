1984697725d710a3de72252cd03e530c35cc1d1f
akliang <none@none.com>
2022-04-18 03:51:43 +0000

    added AddRecipe class

--- api/views/core.py
+++ api/views/core.py
@@ -4,12 +4,13 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
-from api.forms import ModifyRecipe
+from api.forms import RecipeForm
 from api.helpers.cooklang import cooklang_processor as clprocess
 from api.helpers.cooklang import write_formdata_to_cookfile
 from api.views.authentication import lookup_user_by_api
 from rest_framework.views import APIView
 import os
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import BasicAuthentication, SessionAuthentication, TokenAuthentication
@@ -68,9 +69,7 @@ class MyRecipes(APIView):
   permission_classes = [IsAuthenticated]
 
   def get(self, request, *args, **kw):
-    auth = get_authorization_header(request).split()
-    api_key = auth[1].decode()
-    user = lookup_user_by_api(api_key)
+    user = lookup_user_by_api(request)
     recipes = []
     userpath = f"./data/recipes/{user.username}/"
     for filename in os.listdir(userpath):
@@ -80,3 +79,21 @@ class MyRecipes(APIView):
         'url': f'{user.username}/{filename.replace(".cook","")}'
       })
     return Response(recipes)
+
+class AddRecipe(APIView):
+  authentication_classes = [TokenAuthentication]
+  permission_classes = [IsAuthenticated]
+
+  def post(self, request, *args, **kw):
+    user = lookup_user_by_api(request)
+    form = RecipeForm(request.POST)
+    print(form.errors)
+    # write the form data to cook file
+    if form.is_valid():
+      filename = write_formdata_to_cookfile(user, form.cleaned_data)
+      return Response({
+        'username': user.username,
+        'filename': filename
+      }, status=status.HTTP_201_CREATED)
+    else:
+      return Response(form.errors, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
