1a623b39baf9fadc72fd7b59ac780617e5220bf5
akliang <none@none.com>
2022-04-21 13:25:47 +0000

    added registration URL

--- frontend/constants.js
+++ frontend/constants.js
@@ -1,6 +1,7 @@
 const api_url = 'https://cookbook.albertliang.xyz/api';
 
 exports.api_login_url = api_url + '/api_login/';
+exports.api_register_url = api_url + '/api_register/';
 exports.api_viewrecipes_url = api_url + '/view/';
 exports.api_viewrecipesbytoken_url = api_url + '/view_by_token/';
 exports.api_addrecipe_url = api_url + '/add/';
