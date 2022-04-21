d5f09e86593e47f6d6a15d1162bec10abb73ce46
akliang <none@none.com>
2022-04-21 13:26:29 +0000

    added registration view

--- api/views/authentication.py
+++ api/views/authentication.py
@@ -2,11 +2,16 @@ from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.authentication import get_authorization_header
+from django.contrib.auth.models import User
+from django.views import View
+from api.forms import ChefCreationForm
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.views import APIView
+from rest_framework import status
+
 
 class LoginTokenAuth(ObtainAuthToken):
-  # TODO: create user folder on create
-  # TODO: enable logging
-
   def post(self, request, *args, **kwargs):
     serializer = self.serializer_class(data=request.data,
                                         context={'request': request})
@@ -17,6 +22,20 @@ class LoginTokenAuth(ObtainAuthToken):
         'token': token.key,
         # 'username': user.username,
     })
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class RegisterAccount(APIView):
+  def post(self, request, *args, **kw):
+    form = ChefCreationForm(request.POST)
+    print(request.POST)
+    print(form.is_valid())
+    if form.is_valid():
+      form.save()
+      # TODO: create folder/account?
+      return Response(f"User {request.POST['username']} has been registered.")
+    else:
+      return Response(form.errors, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
   
 # helper function to convert API key into username
