e9c85414bfed97898c57e5c9f9ad143fe12cf3ed
akliang <none@none.com>
2022-04-22 04:21:45 +0000

    added recipe model

---
+++ api/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Recipe(models.Model):
+    chef = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    slug = models.CharField(max_length=200)
+    tags = models.CharField(max_length=200, blank=True)
+    recipe = models.TextField()
+    image = models.CharField(max_length=200, blank=True)
