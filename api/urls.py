23513b84ba78a39c5892b8eb6336396a94edaa4b
akliang <none@none.com>
2022-04-24 09:42:28 +0000

    added bookmarks, refactored names to follow python convention

--- api/urls.py
+++ api/urls.py
@@ -1,22 +1,16 @@
 from django.urls import path
 
 from api import views
+from api.views.core import MyBookmarks
 
 urlpatterns = [
-  # path('', views.index, name='index'),
   path('api_login/', views.LoginTokenAuth.as_view(), name='api_login'),
   path('api_register/', views.RegisterAccount.as_view(), name='api_register'),
-  # path('login/', views.login, name='login'),
-  # path('api_login/', views.ApiLogin.as_view(), name='api_login'),
-  # path('logout/', views.logout, name='logout'),
-  # path('register/', views.register, name='register'),
-  # path('recipe/', views.recipe, name='recipe'),
-  # path('v/<str:username>/<str:filename>', views.view, name='view'),
-  # path('view/<str:user>/', views.MyRecipesView.as_view(), name='MyRecipesView'),
-  path('view/<str:username>/<str:slug>', views.RecipeView.as_view(), name='RecipeView'),
-  path('view_by_token/<str:slug>', views.GetRecipeWithToken.as_view(), name='GetRecipeWithToken'),
-  # path('view/<str:user>/<str:recipe>', views.recipe_view, name='temp'),
-  path('view/', views.MyRecipes.as_view(), name='MyRecipes'),
-  path('add/', views.AddRecipe.as_view(), name='Addrecipe'),
-  path('delete/<str:slug>', views.DeleteRecipe.as_view(), name='DeleteRecipe'),
+  path('view/<str:username>/<str:slug>', views.RecipeView.as_view(), name='view_recipe'),
+  path('view_by_token/<str:slug>', views.GetRecipeWithToken.as_view(), name='get_recipe_with_token'),
+  path('view/', views.MyRecipes.as_view(), name='my_recipes'),
+  path('add/', views.AddRecipe.as_view(), name='add_recipe'),
+  path('delete/<str:slug>', views.DeleteRecipe.as_view(), name='delete_recipe'),
+  path('bookmark/', views.BookmarkRecipe.as_view(), name='bookmark_recipe'),
+  path('view_bookmarks/', views.MyBookmarks.as_view(), name='my_bookmarks')
 ]
