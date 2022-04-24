06d58da77a734ea64a4e1d45ec6ce13f29d6e81d
akliang <none@none.com>
2022-04-24 09:41:56 +0000

    added bookmarks

--- api/models.py
+++ api/models.py
@@ -1,3 +1,4 @@
+from operator import mod
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -8,3 +9,7 @@ class Recipe(models.Model):
     tags = models.CharField(max_length=200, blank=True)
     recipe = models.TextField()
     image = models.CharField(max_length=200, blank=True)
+
+class Bookmark(models.Model):
+    chef = models.ForeignKey(User, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
