579688b1312ad18a7d535703c15856d5ed4c1dbb
akliang <none@none.com>
2022-04-28 04:04:59 +0000

    added logging

--- api/views/authentication.py
+++ api/views/authentication.py
@@ -1,27 +1,36 @@
+from django.http import JsonResponse
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.forms import PasswordChangeForm
+
+from rest_framework import status
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
-from django.http import JsonResponse
 from rest_framework.authentication import get_authorization_header
-from api.forms import ChefCreationForm
-from django.utils.decorators import method_decorator
-from django.views.decorators.csrf import csrf_exempt
 from rest_framework.views import APIView
-from rest_framework import status
-from django.contrib.auth.forms import PasswordChangeForm
+
+from api.forms import ChefCreationForm
+
+import logging
+logger = logging.getLogger(__name__)
 
 
 class LoginTokenAuth(ObtainAuthToken):
   def post(self, request, *args, **kwargs):
     serializer = self.serializer_class(data=request.data,
-                                        context={'request': request})
-    serializer.is_valid(raise_exception=True)
-    user = serializer.validated_data['user']
-    token, created = Token.objects.get_or_create(user=user)
-    return Response({
-        'token': token.key,
-        # 'username': user.username,
-    })
+                                       context={'request': request})
+    # serializer.is_valid(raise_exception=True)
+    if serializer.is_valid():
+      user = serializer.validated_data['user']
+      token, created = Token.objects.get_or_create(user=user)
+      if created:
+        logger.info(f"{self.__class__.__name__} - API token created for username {user.username}")
+
+      return Response({'token': token.key})
+    else:
+      logger.warning(f"{self.__class__.__name__} - Failed login attempt for username {request.data['username']} - reason was {serializer.errors}")
+      return Response(status=status.HTTP_401_UNAUTHORIZED)
 
 
 @method_decorator(csrf_exempt, name='dispatch')
@@ -30,18 +39,24 @@ class RegisterAccount(APIView):
     form = ChefCreationForm(request.POST)
     if form.is_valid():
       form.save()
+      logger.info(f"{self.__class__.__name__} - New account registered for username {request.POST['username']}")
       return Response(f"User {request.POST['username']} has been registered.")
     else:
       return Response(form.errors, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 class DeleteAccount(APIView):
   def post(self, request, *args, **kw):
     user = lookup_user_by_api(request)
     if user:
       user.delete()
+      logger.info(f"{self.__class__.__name__} - Account deleted for username {user.username}")
       return JsonResponse({'username': user.username})
     else:
-      return Response("User account not found.")
+      apikey = parse_apikey_from_header(request)
+      logger.warning(f"{self.__class__.__name__} - Attempted account deletion failed for API key {apikey}")
+      return Response("User account not found.", status=status.HTTP_403_FORBIDDEN)
+
 
 class ChangeAccountPassword(APIView):
   def post(self, request, *args, **kw):
@@ -52,20 +67,30 @@ class ChangeAccountPassword(APIView):
         form.save()
         # delete the API token to force a new key on next login
         Token.objects.get(user=user).delete()
+        logger.info(f"{self.__class__.__name__} - Password successfully changed for username {user.username}")
         return Response("Password updated.")
       else:
         return Response(form.errors, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     else:
-      return Response("Something went wrong - password not changed.")
+      apikey = parse_apikey_from_header(request)
+      logger.warning(f"{self.__class__.__name__} - Invalid password reset attempt using API key {apikey}")
+      return Response("Something went wrong - password not changed.", status=status.HTTP_403_FORBIDDEN)
 
   
-# helper function to convert API key into username
-def lookup_user_by_api(request):
+# helper function to grab the API key from the header
+def parse_apikey_from_header(request):
   auth = get_authorization_header(request).split()
-  auth_token = auth[1].decode()
-  if auth_token != 'undefined' and auth_token != 'null':
-    api_key = auth[1].decode()
-    token = Token.objects.get(key=api_key)
+  apikey = auth[1].decode()
+  if apikey == 'undefined' or apikey == 'null':
+    return None
+  else:
+    return apikey
+
+# helper function to translate the apikey into a User object
+def lookup_user_by_api(request):
+  apikey = parse_apikey_from_header(request)
+  if apikey:
+    token = Token.objects.get(key=apikey)
     return token.user
   else:
     return None
--- settings/settings.py
+++ settings/settings.py
@@ -98,6 +98,44 @@ DATABASES = {
     }
 }
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} [{module}] {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+        },
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs', 'debug.log'),
+            'formatter': 'verbose',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+    'loggers': {
+        'api': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
